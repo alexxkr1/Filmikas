85497eaa70cb2dcd2a1a17783a9f5dde5ef60c1a
Opilane TTHK <opilane@tthk.ee>
2022-11-14 13:44:51 +0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+
+const app = require("express")();
+const port = 3000;
+const express = require('express');
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server up at http://localhost:${port}`)
+});
