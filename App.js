c0cb5337cf0d2833843687bb95ec73c6d4605a83
Opilane TTHK <opilane@tthk.ee>
2022-11-15 12:54:31 +0200

    Init

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ const app = require("express")();
 const port = 3000;
 const express = require('express');
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
-
+require("./routes/routes")(app);
 
 app.listen(port, () => {
     console.log(`Server up at http://localhost:${port}`)
